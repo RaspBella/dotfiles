4c7550cfa437eafe7c58a65f53501620acd62309
Bella Campbell <Bella.Campbell@tuta.io>
2024-07-14 02:15:33 +0100

    picom

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -577,5 +577,5 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.spawn.with_shell("compton --vsync")
+awful.spawn.with_shell("picom --vsync")
 awful.spawn.with_shell(terminal .. " -e killall " .. sound_player .. " cbatticon && cbatticon && " .. play_startup_sound)
