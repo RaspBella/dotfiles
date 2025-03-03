8d1fbf5e902c14d284e6fa535c3f127890033cdb
Bella Campbell <Bella.Campbell@tuta.io>
2025-03-03 19:41:55 +0000

    But heres the train

---
+++ .config/waybar/train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import requests
+from datetime import date
+
+today = date.today().isoformat()
+
+data = requests.get("https://bella.voklen.com/trans/data.json").json()
+
+for date in data:
+    if date >= today:
+        print(date)
+        exit()
+
+print("next")
